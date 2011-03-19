38bc1905144d42de76daa424f7a4b4b572677fc1
Jean Boussier <jean.boussier@dimelo.com>
2011-03-19 11:02:17 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RssExpander::Application.config.secret_token = '83db37aa2081da3fdb4f1ee83a3008527ff96d7fd40f480d6bcf7f12a3fb12fb7ee241425824ff1f640ff2a8ccaa968a41510f224d487963ba090cd907b5cbc5'
